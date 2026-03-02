766b04d500f5334b755b50b710fec4ac8df53a33
St John, Austin <pokeboy7to8@gmail.com>
2026-03-01 19:27:38 -0800

    Finished (this is the stars one)

--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -1,10 +1,27 @@
-// Chapter 5, Programming Challenge 17: Sales Bar Chart
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
-int main()
-{
+int main() {
+    
+    int sales; 
+   int store [5];
+   for (int number = 0; number < 5; number++){
+      cout << "Enter today's sales for store"<< (number + 1)<< ": ";
+      cin >> store[number];
+   }
+   cout << " SALES BAR CHART" <<endl;
+   cout << "(Each * = $100)" <<endl;
 
-return 0;
+   for(int number = 0; number < 5; number++){
+      cout <<"Store " << (number + 1 )<< ": ";
+      int stars = store[number] / 100;
+      for (int i = 0; i < stars; i++){
+         cout<< "*";
+      }
+      cout<<endl;
+   }
+   
+
+   return 0;
 }
