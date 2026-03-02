6cd14b9a05b590218a854a0227a589f694deb290
St John, Austin <pokeboy7to8@gmail.com>
2026-03-01 19:26:37 -0800

    Finished (this is the numbers assignment)

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 int main() {
 
-   /* Type your code here. */
+   double num1;
+   double num2;
+
+   cin >> num1;
+   cin >> num2;
+
+   if(num2 < num1){
+      cout<< "Second integer can't be less than the first." << endl;
+   }
+   else{
+      for(int i = num1; i <= num2; i+= 5){
+         cout<< i << " ";
+      }
+      cout<<endl;
+   }
+   
 
    return 0;
 }
